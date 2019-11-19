ae430b269bf2ea4439ea24bc9559443b996f091c
QuyenLH <quyenlh@vnext.com.vn>
2019-11-19 15:18:53 +0700

    add url tiki, sendo

---
+++ CEDTeam.CES.Core/Dtos/TikiHotSearchDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CEDTeam.CES.Core.Dtos
+{
+    public class Datum
+    {
+        public int updated_at { get; set; }
+        public string image_url { get; set; }
+        public string keyword { get; set; }
+        public int points { get; set; }
+    }
+
+    public class TikiHotSearchDto
+    {
+        public List<Datum> data { get; set; }
+        public int is_personalized { get; set; }
+    }
+}
