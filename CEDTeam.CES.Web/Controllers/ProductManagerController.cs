e0caab1a144701a51e7962a3bc851a914395b1d8
Phuong Nguyen <Sjungok@outlook.com>
2019-11-23 16:25:18 +0700

    Spit Product Manager

--- CEDTeam.CES.Web/Controllers/ProductManagerController.cs
+++ CEDTeam.CES.Web/Controllers/ProductManagerController.cs
@@ -15,9 +15,17 @@ namespace CEDTeam.CES.Web.Controllers
         {
             _productService = productService;
         }
-        public IActionResult Index()
+        public IActionResult Index(int? siteId)
         {
-            return View();
+            ViewBag.Title = "Quản lý sản phẩm";
+            switch (siteId)
+            {
+                case 1: ViewBag.Title = "Quản lý sản phẩm - Shopee"; break;
+                case 2: ViewBag.Title = "Quản lý sản phẩm - Lazada"; break;
+                case 3: ViewBag.Title = "Quản lý sản phẩm - Tiki"; break;
+                case 4: ViewBag.Title = "Quản lý sản phẩm - Sendo"; break;
+            }
+            return View(siteId);
         }
 
         [Route("get-product")]
@@ -31,6 +39,66 @@ namespace CEDTeam.CES.Web.Controllers
             int columnOrder = int.Parse(Request.Form["order[0][column]"]);
             bool isAsc = "asc".Equals(Request.Form["order[0][dir]"]);
             var result = await _productService.GetProductAsync(start, length, search, columnOrder, isAsc);
+            result.Draw = draw;
+            return new ObjectResult(result);
+        }
+
+        [Route("get-shopee-product")]
+        [HttpPost]
+        public async Task<IActionResult> GetShopeeProduct()
+        {
+            int draw = int.Parse(Request.Form["draw"]);
+            int start = int.Parse(Request.Form["start"]);
+            int length = int.Parse(Request.Form["length"]);
+            string search = Request.Form["search[value]"];
+            int columnOrder = int.Parse(Request.Form["order[0][column]"]);
+            bool isAsc = "asc".Equals(Request.Form["order[0][dir]"]);
+            var result = await _productService.GetProductWithSiteIdAsync(start, length, search, columnOrder, 1, isAsc);
+            result.Draw = draw;
+            return new ObjectResult(result);
+        }
+
+        [Route("get-lazada-product")]
+        [HttpPost]
+        public async Task<IActionResult> GetLazadaProduct()
+        {
+            int draw = int.Parse(Request.Form["draw"]);
+            int start = int.Parse(Request.Form["start"]);
+            int length = int.Parse(Request.Form["length"]);
+            string search = Request.Form["search[value]"];
+            int columnOrder = int.Parse(Request.Form["order[0][column]"]);
+            bool isAsc = "asc".Equals(Request.Form["order[0][dir]"]);
+            var result = await _productService.GetProductWithSiteIdAsync(start, length, search, columnOrder, 2, isAsc);
+            result.Draw = draw;
+            return new ObjectResult(result);
+        }
+
+        [Route("get-tiki-product")]
+        [HttpPost]
+        public async Task<IActionResult> GetTikiProduct()
+        {
+            int draw = int.Parse(Request.Form["draw"]);
+            int start = int.Parse(Request.Form["start"]);
+            int length = int.Parse(Request.Form["length"]);
+            string search = Request.Form["search[value]"];
+            int columnOrder = int.Parse(Request.Form["order[0][column]"]);
+            bool isAsc = "asc".Equals(Request.Form["order[0][dir]"]);
+            var result = await _productService.GetProductWithSiteIdAsync(start, length, search, columnOrder, 3, isAsc);
+            result.Draw = draw;
+            return new ObjectResult(result);
+        }
+
+        [Route("get-sendo-product")]
+        [HttpPost]
+        public async Task<IActionResult> GetSendoProduct()
+        {
+            int draw = int.Parse(Request.Form["draw"]);
+            int start = int.Parse(Request.Form["start"]);
+            int length = int.Parse(Request.Form["length"]);
+            string search = Request.Form["search[value]"];
+            int columnOrder = int.Parse(Request.Form["order[0][column]"]);
+            bool isAsc = "asc".Equals(Request.Form["order[0][dir]"]);
+            var result = await _productService.GetProductWithSiteIdAsync(start, length, search, columnOrder, 4, isAsc);
             result.Draw = draw;
             return new ObjectResult(result);
         }
