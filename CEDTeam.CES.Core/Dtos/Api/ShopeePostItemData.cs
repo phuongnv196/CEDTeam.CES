977d8c78b9bb1ec2eced87de762149e48b3f5ed0
QuyenLH <quyenlh@vnext.com.vn>
2020-03-30 21:49:00 +0700

    Fix get product shopee thiếu data

---
+++ CEDTeam.CES.Core/Dtos/Api/ShopeePostItemData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CEDTeam.CES.Core.Dtos
+{
+    public class ShopeeItem
+    {
+        public long itemid { get; set; }
+        public long shopid { get; set; }
+    }
+    public class ShopeePostItemData
+    {
+        public List<ShopeeItem> item_shop_ids;
+    }
+}
---
+++ CEDTeam.CES.Core/Dtos/Api/ShopeeSearchItemv1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CEDTeam.CES.Core.Dtos.Api
+{
+    public class QueryRewrite
+    {
+    }
+
+    public class Adjust
+    {
+        public int count { get; set; }
+    }
+
+    public class PriceAdjust
+    {
+        public int count { get; set; }
+    }
+
+    public class ShopeeSearchItemv1
+    {
+        public bool? show_disclaimer { get; set; }
+        public QueryRewrite query_rewrite { get; set; }
+        public Adjust adjust { get; set; }
+        public string version { get; set; }
+        public string algorithm { get; set; }
+        public int total_count { get; set; }
+        public List<ShopeeItem> items { get; set; }
+        public int total_ads_count { get; set; }
+        public string reserved_keyword { get; set; }
+        public PriceAdjust price_adjust { get; set; }
+        public int suggestion_algorithm { get; set; }
+        public List<object> hint_keywords { get; set; }
+        public bool? nomore { get; set; }
+        public string json_data { get; set; }
+    }
+}
